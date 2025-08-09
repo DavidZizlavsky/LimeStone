2843a39060391c0829603ed9d8b681a8b8820e39
David Žižlavský <zizlavsky_david@seznam.cz>
2025-08-09 15:48:25 +0200

    Validation layers

--- LimeStone/include/LimeStone/LimeStone.h
+++ LimeStone/include/LimeStone/LimeStone.h
@@ -1,15 +1,23 @@
 #pragma once
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
+#include <vector>
 
 namespace LimeStone {
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, 
+		VkDebugUtilsMessageTypeFlagsEXT messageType, 
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, 
+		void* pUserData);
 	class Application {
 	private:
 		GLFWwindow* m_window = nullptr;
 		VkInstance m_vkInstance = nullptr;
+		VkDebugUtilsMessengerEXT m_vkDebugMessenger = nullptr;
 
 		void initVulkan();
 		bool checkValidationLayerSupport();
+		std::vector<const char*> getRequiredExtensions();
+		
 	public:
 		Application();
 		~Application();
